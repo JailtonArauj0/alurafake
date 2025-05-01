7d2b5bcc0a52bc41b417c2b907d260994f7a335d
Jailton Ramos <jailton.ramos@omnilink.com.br>
2025-05-01 09:55:55 -0300

    fix: desabilita corretamente o csrf e permite requisições de criação e login sem autenticação

--- src/main/java/br/com/alura/AluraFake/security/SecurityConfig.java
+++ src/main/java/br/com/alura/AluraFake/security/SecurityConfig.java
@@ -2,10 +2,12 @@ package br.com.alura.AluraFake.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -28,12 +30,11 @@ public class SecurityConfig {
         httpSecurity.authorizeHttpRequests(
                 auth -> {
                     auth
-                        .requestMatchers("/user/new").permitAll()
-                        .requestMatchers("/user/all").permitAll()
-                        .anyRequest().authenticated();
+                            .requestMatchers("/user/new", "/user/login").permitAll()
+                            .anyRequest().authenticated();
                 })
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
-                .csrf(CsrfConfigurer::disable)
+                .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return httpSecurity.build();
