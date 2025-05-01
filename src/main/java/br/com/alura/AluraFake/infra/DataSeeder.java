a880147811e09c269473b57d8e1f1d5a857ecab5
Jailton Ramos <jailton.ramos@omnilink.com.br>
2025-05-01 10:46:33 -0300

    feat: remove geração automática de senha e codifica senha na criação de usuário

--- src/main/java/br/com/alura/AluraFake/infra/DataSeeder.java
+++ src/main/java/br/com/alura/AluraFake/infra/DataSeeder.java
@@ -1,9 +1,12 @@
 package br.com.alura.AluraFake.infra;
 
 import br.com.alura.AluraFake.course.*;
+import br.com.alura.AluraFake.repositories.CourseRepository;
+import br.com.alura.AluraFake.repositories.UserRepository;
 import br.com.alura.AluraFake.user.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -25,10 +28,10 @@ public class DataSeeder implements CommandLineRunner {
     @Override
     public void run(String... args) {
         if (!"dev".equals(activeProfile)) return;
-
+        // REMOVI A GERAÇÃO AUTOMATICA DE SENHA PARA FACILITAR O LOGIN, DEVIDO AO HASH DA SENHA NO BANCO
         if (userRepository.count() == 0) {
-            User caio = new User("Caio", "caio@alura.com.br", Role.STUDENT);
-            User paulo = new User("Paulo", "paulo@alura.com.br", Role.INSTRUCTOR);
+            User caio = new User("Caio", "caio@alura.com.br", Role.STUDENT, new BCryptPasswordEncoder().encode("123456"));
+            User paulo = new User("Paulo", "paulo@alura.com.br", Role.INSTRUCTOR, new BCryptPasswordEncoder().encode("123456"));
             userRepository.saveAll(Arrays.asList(caio, paulo));
             courseRepository.save(new Course("Java", "Aprenda Java com Alura", paulo));
         }
