7f8fcc5ba63437ff8ccc9340f9eda10f5721b711
Jailton Ramos <jailton.ramos@omnilink.com.br>
2025-04-30 21:19:31 -0300

    feat: criada EntityNotFoundException para lidar com entidades não encontradas

---
+++ src/main/java/br/com/alura/AluraFake/exception/EntityNotFoundException.java
@@ -0,0 +1,11 @@
+package br.com.alura.AluraFake.exception;
+
+public class EntityNotFoundException extends RuntimeException{
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
+    public EntityNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
