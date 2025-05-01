eede77998ea66b55f09d6d89991ba9a0fd72ba73
Jailton Ramos <jailton.ramos@omnilink.com.br>
2025-05-01 09:58:28 -0300

    feat: implementa endpoint de login com autenticação e geração de token

--- src/main/java/br/com/alura/AluraFake/user/UserController.java
+++ src/main/java/br/com/alura/AluraFake/user/UserController.java
@@ -1,8 +1,14 @@
 package br.com.alura.AluraFake.user;
 
+import br.com.alura.AluraFake.dtos.LoginRequestDTO;
+import br.com.alura.AluraFake.dtos.LoginResponseDTO;
+import br.com.alura.AluraFake.security.TokenService;
 import br.com.alura.AluraFake.util.ErrorItemDTO;
 import jakarta.validation.Valid;
 import org.springframework.http.*;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,9 +18,13 @@ import java.util.List;
 public class UserController {
 
     private final UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
 
-    public UserController(UserRepository userRepository) {
+    public UserController(UserRepository userRepository, AuthenticationManager authenticationManager, TokenService tokenService) {
         this.userRepository = userRepository;
+        this.authenticationManager = authenticationManager;
+        this.tokenService = tokenService;
     }
 
     @Transactional
@@ -32,6 +42,15 @@ public class UserController {
     @GetMapping("/user/all")
     public List<UserListItemDTO> listAllUsers() {
         return userRepository.findAll().stream().map(UserListItemDTO::new).toList();
+    }
+
+    @PostMapping("/user/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid LoginRequestDTO loginRequestDTO) {
+        var usernamePassword = new UsernamePasswordAuthenticationToken(loginRequestDTO.getEmail(), loginRequestDTO.getPassword());
+        var auth = authenticationManager.authenticate(usernamePassword);
+
+        var token = tokenService.generateToken((User) auth.getPrincipal());
+        return ResponseEntity.ok(new LoginResponseDTO(token));
     }
 
 }
