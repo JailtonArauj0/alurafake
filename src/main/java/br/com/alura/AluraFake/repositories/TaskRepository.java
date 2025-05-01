55f3cac3d3fb710cafc7fc73be8d52ac2f15034d
Jailton Ramos <jailton.ramos@omnilink.com.br>
2025-05-01 19:58:35 -0300

    feat: criado o TaskRepository

---
+++ src/main/java/br/com/alura/AluraFake/repositories/TaskRepository.java
@@ -0,0 +1,17 @@
+package br.com.alura.AluraFake.repositories;
+
+import br.com.alura.AluraFake.course.Course;
+import br.com.alura.AluraFake.task.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    Optional<Task> findTaskByStatement(String statement);
+
+    List<Task> findAllByCourseIdOrderByTaskOrder(Course courseId);
+}
