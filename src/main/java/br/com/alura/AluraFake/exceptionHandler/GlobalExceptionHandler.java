ad69236ad87eb0e03dfcdc29106a6565fcb5b28d
Jailton Ramos <jailton.ramos@omnilink.com.br>
2025-05-01 22:04:14 -0300

    feat: implementa tratamento de exceções para validação de argumentos em métodos

--- src/main/java/br/com/alura/AluraFake/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/br/com/alura/AluraFake/exceptionHandler/GlobalExceptionHandler.java
@@ -6,12 +6,16 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -38,6 +42,25 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         problem.setStatus(status.value());
 
         return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        List<Problem.Field> fields = new ArrayList<>();
+        for (var error : ex.getBindingResult().getAllErrors()) {
+            String name = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+
+            fields.add(new Problem.Field(name, message));
+        }
+
+        Problem problem = new Problem(LocalDateTime.now());
+        problem.setStatus(status.value());
+        problem.setTitle("One or more fields are invalid, please fill them correctly!");
+        problem.setFields(fields);
+
+
+        return handleExceptionInternal(ex, problem, headers, status, request);
     }
 
 }
