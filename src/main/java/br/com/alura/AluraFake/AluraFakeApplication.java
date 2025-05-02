4e1b56ab4f38426844d7fcc156777d2c4290f04c
Jailton Ramos <jailton.ramos@omnilink.com.br>
2025-05-02 20:06:54 -0300

    feat: padroniza locale do sistema para inglês

--- src/main/java/br/com/alura/AluraFake/AluraFakeApplication.java
+++ src/main/java/br/com/alura/AluraFake/AluraFakeApplication.java
@@ -3,10 +3,14 @@ package br.com.alura.AluraFake;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+
 @SpringBootApplication
 public class AluraFakeApplication {
 
 	public static void main(String[] args) {
+		// PADRONIZEI O LOCALE PARA INGLÊS DEVIDO A MENSAGENS DE TRATAMENTO DE ERRO PADRÃO DO SPRING
+		Locale.setDefault(Locale.ENGLISH);
 		SpringApplication.run(AluraFakeApplication.class, args);
 	}
 
