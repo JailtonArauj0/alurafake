16a46fdd32d093193384e05603bc1c74000fa2a8
Jailton Ramos <jailton.ramos@omnilink.com.br>
2025-05-01 22:06:55 -0300

    feat: cria o DTO ChoiceDTO e Option com validações

---
+++ src/main/java/br/com/alura/AluraFake/dtos/request/ChoiceDTO.java
@@ -0,0 +1,81 @@
+package br.com.alura.AluraFake.dtos.request;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+import java.util.Objects;
+
+public class ChoiceDTO {
+    @NotNull
+    private Long courseId;
+
+    @NotNull
+    @Size(min = 4, max = 255, message = "Statement must be between 4 and 255 characters")
+    private String statement;
+
+    @NotNull
+    @Min(value = 1, message = "Order must be an integer and greater than 0")
+    private Integer order;
+
+    @NotNull
+    @Size(min = 2, max = 5, message = "The number of options must be between 2 and 5")
+    @Valid
+    private List<Option> options;
+
+    public ChoiceDTO() {
+    }
+
+    public ChoiceDTO(Long courseId, String statement, Integer order, List<Option> options) {
+        this.courseId = courseId;
+        this.statement = statement;
+        this.order = order;
+        this.options = options;
+    }
+
+    public Long getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Long courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getStatement() {
+        return statement;
+    }
+
+    public void setStatement(String statement) {
+        this.statement = statement;
+    }
+
+    public Integer getOrder() {
+        return order;
+    }
+
+    public void setOrder(Integer order) {
+        this.order = order;
+    }
+
+    public List<Option> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<Option> options) {
+        this.options = options;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ChoiceDTO choiceDTO = (ChoiceDTO) o;
+        return Objects.equals(courseId, choiceDTO.courseId) && Objects.equals(statement, choiceDTO.statement) && Objects.equals(order, choiceDTO.order) && Objects.equals(options, choiceDTO.options);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(courseId, statement, order, options);
+    }
+}
---
+++ src/main/java/br/com/alura/AluraFake/dtos/request/Option.java
@@ -0,0 +1,51 @@
+package br.com.alura.AluraFake.dtos.request;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.Objects;
+
+public class Option {
+    @NotNull
+    @Size(min = 4, max = 255, message = "Option must be between 4 and 80 characters")
+    private String option;
+
+    @NotNull
+    private Boolean isCorrect;
+
+    public Option() {
+    }
+
+    public Option(String option, Boolean isCorrect) {
+        this.option = option;
+        this.isCorrect = isCorrect;
+    }
+
+    public String getOption() {
+        return option;
+    }
+
+    public void setOption(String option) {
+        this.option = option;
+    }
+
+    public Boolean isCorrect() {
+        return isCorrect;
+    }
+
+    public void setIsCorrect(Boolean correct) {
+        isCorrect = correct;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Option option1 = (Option) o;
+        return Objects.equals(option, option1.option) && Objects.equals(isCorrect, option1.isCorrect);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(option, isCorrect);
+    }
+}
