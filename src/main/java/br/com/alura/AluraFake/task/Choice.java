a9075ba03eb308adfc07e33eb6e24a7ee2f8a5e3
Jailton Ramos <jailton.ramos@omnilink.com.br>
2025-05-02 20:02:45 -0300

    feat: adiciona getters, setters e construtores

--- src/main/java/br/com/alura/AluraFake/task/Choice.java
+++ src/main/java/br/com/alura/AluraFake/task/Choice.java
@@ -19,4 +19,45 @@ public class Choice {
     @ManyToOne
     @JoinColumn(name = "task_id")
     private Task task;
+
+    public Choice() {
+    }
+
+    public Choice(String optionText, Boolean isCorrect, Task task) {
+        this.optionText = optionText;
+        this.isCorrect = isCorrect;
+        this.task = task;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOptionText() {
+        return optionText;
+    }
+
+    public void setOptionText(String optionText) {
+        this.optionText = optionText;
+    }
+
+    public Boolean getCorrect() {
+        return isCorrect;
+    }
+
+    public void setCorrect(Boolean correct) {
+        isCorrect = correct;
+    }
+
+    public Task getTask() {
+        return task;
+    }
+
+    public void setTask(Task task) {
+        this.task = task;
+    }
 }
