78cfed80d80d7b4754d33533ce64535501c145cd
Jailton Ramos <jailton.ramos@omnilink.com.br>
2025-05-04 09:43:29 -0300

    feat: cria getters e setters

--- src/main/java/br/com/alura/AluraFake/domain/model/course/Course.java
+++ src/main/java/br/com/alura/AluraFake/domain/model/course/Course.java
@@ -37,31 +37,55 @@ public class Course {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public LocalDateTime getCreatedAt() {
         return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public void setStatus(Status status) {
-        this.status = status;
-    }
-
-    public User getInstructor() {
-        return instructor;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getDescription() {
         return description;
     }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(User instructor) {
+        this.instructor = instructor;
+    }
+
     public Status getStatus() {
         return status;
     }
 
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
     public LocalDateTime getPublishedAt() {
         return publishedAt;
+    }
+
+    public void setPublishedAt(LocalDateTime publishedAt) {
+        this.publishedAt = publishedAt;
     }
 }
