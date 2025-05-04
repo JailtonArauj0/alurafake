bc6710ace0280fe0cc90a3aa822530f48be23f32
Jailton Ramos <jailton.ramos@omnilink.com.br>
2025-05-04 11:55:08 -0300

    feat: adiciona anotação @Repository nas interfaces CourseRepository e UserRepository

--- src/main/java/br/com/alura/AluraFake/domain/repository/CourseRepository.java
+++ src/main/java/br/com/alura/AluraFake/domain/repository/CourseRepository.java
@@ -2,7 +2,9 @@ package br.com.alura.AluraFake.domain.repository;
 
 import br.com.alura.AluraFake.domain.model.course.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CourseRepository extends JpaRepository<Course, Long>{
 
 }
--- src/main/java/br/com/alura/AluraFake/domain/repository/UserRepository.java
+++ src/main/java/br/com/alura/AluraFake/domain/repository/UserRepository.java
@@ -3,9 +3,11 @@ package br.com.alura.AluraFake.domain.repository;
 
 import br.com.alura.AluraFake.domain.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
