37b12f16b3c52b4c27c5b1b02e5ee0a5f09ec007
Jailton Ramos <jailton.ramos@omnilink.com.br>
2025-05-04 22:25:16 -0300

    test: adiciona configuração de teste e ajusta persistência de usuários

--- src/test/java/br/com/alura/AluraFake/domain/repository/UserRepositoryTest.java
+++ src/test/java/br/com/alura/AluraFake/domain/repository/UserRepositoryTest.java
@@ -2,9 +2,11 @@ package br.com.alura.AluraFake.domain.repository;
 
 import br.com.alura.AluraFake.domain.model.user.Role;
 import br.com.alura.AluraFake.domain.model.user.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
@@ -18,12 +20,23 @@ class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @BeforeEach
+    void setUp() {
+        userRepository.deleteAll();
+        entityManager.flush();
+        entityManager.clear();
+    }
+
     @Test
     void findByEmail__should_return_existis_user() {
-        User caio = new User("Caio", "caio@alura.com.br", Role.STUDENT);
-        userRepository.save(caio);
+        User caio = new User("Caio", "caio1@alura.com.br", Role.STUDENT);
+        entityManager.persist(caio);
+        entityManager.flush();
 
-        Optional<User> result = userRepository.findByEmail("caio@alura.com.br");
+        Optional<User> result = userRepository.findByEmail("caio1@alura.com.br");
         assertThat(result).isPresent();
         assertThat(result.get().getName()).isEqualTo("Caio");
 
@@ -33,10 +46,11 @@ class UserRepositoryTest {
 
     @Test
     void existsByEmail__should_return_true_when_user_existis() {
-        User caio = new User("Caio", "caio@alura.com.br", Role.STUDENT);
-        userRepository.save(caio);
+        User caio = new User("Caio", "caio2@alura.com.br", Role.STUDENT);
+        entityManager.persist(caio);
+        entityManager.flush();
 
-        assertThat(userRepository.existsByEmail("caio@alura.com.br")).isTrue();
+        assertThat(userRepository.existsByEmail("caio2@alura.com.br")).isTrue();
         assertThat(userRepository.existsByEmail("sergio@alura.com.br")).isFalse();
     }
 }
